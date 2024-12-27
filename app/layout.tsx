372b698ac0d8be175347ba87a190dd8bbbb8a595
Pawarit Wongdaeng <martin@Martins-MacBook-Pro.local>
2024-12-27 15:52:29 +0700

    Fri 27 Dec 15:52

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,17 +19,17 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-            <SessionProvider>
-                {children}
-            </SessionProvider>
-        </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+                <SessionProvider>
+                    {children}
+                </SessionProvider>
+            </body>
+        </html>
+    );
 }
