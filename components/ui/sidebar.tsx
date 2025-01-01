9cd775f3f05d6ca6ab7f6c0b3db20c1ea0b5194f
Pawarit Wongdaeng <martin@Martins-MacBook-Pro.local>
2025-01-01 10:53:17 +0700

    x

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -250,7 +250,7 @@ const Sidebar = React.forwardRef<
         >
           <div
             data-sidebar="sidebar"
-            className="flex h-full w-full flex-col bg-sidebar group-data-[variant=floating]:rounded-lg group-data-[variant=floating]:border group-data-[variant=floating]:border-sidebar-border group-data-[variant=floating]:shadow"
+            className="flex h-full w-full flex-col bg-white group-data-[variant=floating]:rounded-lg group-data-[variant=floating]:border group-data-[variant=floating]:border-sidebar-border group-data-[variant=floating]:shadow"
           >
             {children}
           </div>
@@ -288,6 +288,7 @@ const SidebarTrigger = React.forwardRef<
             {...props}
         >
             <div className="burger" id="burger">
+                <span></span>
                 <span></span>
                 <span className="burger-top"></span>
                 <span></span>
